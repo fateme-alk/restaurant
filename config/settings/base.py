a053b3997ab3c4dfcb15b2e95b3b77786bfe79cb
Fateme <fateme.alk9@gmail.com>
2024-04-11 22:45:02 +0330

    add items django app

--- config/settings/base.py
+++ config/settings/base.py
@@ -85,6 +85,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "restaurant.users",
+    "restaurant.items",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
