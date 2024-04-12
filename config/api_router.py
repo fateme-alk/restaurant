a8e76b072650c7c15d1ef84cb0ec8127595dcf25
Fateme <fateme.alk9@gmail.com>
2024-04-12 21:49:25 +0330

    add orders route

--- config/api_router.py
+++ config/api_router.py
@@ -2,12 +2,14 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from restaurant.items.api.views import CategoryViewset, ItemViewset
+from restaurant.orders.api.views import OrderViewset
 from restaurant.users.api.views import UserViewSet
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
 router.register("users", UserViewSet)
 router.register("items", ItemViewset)
+router.register("orders", OrderViewset)
 router.register("categories", CategoryViewset)
 
 
