05dc33f6c3b5a5c05f0d9e6fa900b08acedafab2
Fateme <fateme.alk9@gmail.com>
2024-04-12 21:16:26 +0330

    add API routes for handling items and categories in items app

--- config/api_router.py
+++ config/api_router.py
@@ -1,11 +1,14 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
+from restaurant.items.api.views import CategoryViewset, ItemViewset
 from restaurant.users.api.views import UserViewSet
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
 router.register("users", UserViewSet)
+router.register("items", ItemViewset)
+router.register("categories", CategoryViewset)
 
 
 app_name = "api"
